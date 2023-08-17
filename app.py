5cce281f9b617aef14a3e6fb85d455e2b5af7002
Emily Bradley <emily@thisisanemail.com>
2023-08-17 13:21:08 +0000

    emily first app adds

--- app.py
+++ app.py
@@ -8,3 +8,32 @@ db = mysql.connector.connect(
     password="c0nygre",
     database="portfolio"
 )
+
+@app.route('/products', methods=['GET'])
+def gettotalvalue():
+    # Calculates the current total value of the portfolio
+    cursor = db.cursor()
+    cursor.execute('''SELECT SUM(qty*price) as totalValue FROM
+                        (SELECT cash.id, cash.qty, cash.currentValue AS price FROM cash
+                        UNION
+                        SELECT bonds.id, bonds.qty, bonds.currentPrice AS price FROM bonds
+                        UNION
+                        SELECT stocks.id, stocks.qty, stocks.currentPrice AS price FROM stocks) as combinedTable''')
+    products = cursor.fetchall()
+    cursor.close()
+    return jsonify(products)
+
+
+@app.route('/products', methods=['GET'])
+def getinitialvalue():
+    # Calculates the current total value of the portfolio
+    cursor = db.cursor()
+    cursor.execute('''SELECT SUM(qty*price) as initialValue FROM
+                        (SELECT cash.id, cash.qty, cash.currentValue AS price FROM cash
+                        UNION
+                        SELECT bonds.id, bonds.qty, bonds.currentPrice AS price FROM bonds
+                        UNION
+                        SELECT stocks.id, stocks.qty, stocks.currentPrice AS price FROM stocks) as combinedTable''')
+    products = cursor.fetchall()
+    cursor.close()
+    return jsonify(products)
