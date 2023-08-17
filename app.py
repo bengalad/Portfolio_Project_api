62c778559661421fbc40e6640773291d0f4e4d13
Gbenga Ladipo <gbenga@nonsense.com>
2023-08-17 13:17:38 +0000

    Adding method to connect to yahoo finance and a test file

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify, request
 import mysql.connector
+import yfinance as yf
+import pandas as pd
 
 app = Flask(__name__)
 db = mysql.connector.connect(
@@ -8,3 +10,21 @@ db = mysql.connector.connect(
     password="c0nygre",
     database="portfolio"
 )
+
+@app.route('/products/<string:ticker>', methods=['GET'])
+def get_historical_prices(ticker):
+    # Retrieve the item with the given ticker from yahoo finance
+    product = yf.Ticker(ticker)
+    
+    # getting historical market data
+    hist = product.history(period="1mo")
+    
+    if not hist.empty:
+        # Return the item as JSON
+        return hist.to_json()
+    else:
+        return jsonify({"Error":"Ticker data not found"}), 404
+    
+
+if __name__ == '__main__':
+    app.run()
