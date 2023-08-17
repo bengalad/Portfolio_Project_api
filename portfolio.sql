f0042aec95c27662a9e311e282ad6074bd93b706
Helena <helena@nonsense.com>
2023-08-17 12:38:23 +0000

    added current price column

--- portfolio.sql
+++ portfolio.sql
@@ -7,6 +7,7 @@ CREATE TABLE holdings
    id          varchar(11) 	    NOT NULL,
    holdingName       varchar(40)       NOT NULL,
    holdingType       varchar(20)       NOT NULL,
+	ticker		varchar(5) 	    NOT NULL,
    PRIMARY KEY(id)
 );
 
@@ -17,6 +18,7 @@ CREATE TABLE stocks
    dateOfPurchase           date           NULL,
    priceAtPurchase          decimal(10,2)               NULL,
    qty        decimal(10,2)           NULL ,
+   currentPrice  decimal(10,2) 			NULL,
    PRIMARY KEY(id)
 );
 
@@ -29,6 +31,7 @@ CREATE TABLE bonds
    qty        decimal(10,2)           NULL ,
    parValue          decimal(10,2)               NULL,
    maturityDate           date           NULL,
+   currentPrice  decimal(10,2) 			NULL,
    PRIMARY KEY(id)
 );
 
@@ -40,48 +43,49 @@ CREATE TABLE cash
    exchAtPurchase          decimal(10,2)               NULL,
    exchCurrent          decimal(10,2)               NULL,
    qty        decimal(10,2)           NULL ,
+   currentValue  decimal(10,2) 			NULL,
    PRIMARY KEY(id)
 );
 
 
 insert stocks
    values('1', 'Stock1', '2023-10-01', 10.99,
-   10);
+   10, 12.99);
    insert stocks
    values('2', 'Stock2', '2023-10-11', 9.99,
-   6);
+   6, 10.99);
    insert stocks
    values('3', 'Stock3', '2023-10-05', 15.99,
-   50);
+   50, 11.99);
 
 insert bonds
    values('4', 'Bond1', '2023-10-01', 10.99,
-   10, 100, '2025-10-01');
+   10, 100, '2025-10-01', 10.99);
 insert bonds
    values('5', 'Bond2', '2023-8-11', 3.99,
-   8, 300, '2024-10-01');
+   8, 300, '2024-10-01', 2.99);
 insert bonds
    values('6', 'Bond3', '2023-8-05', 7.99,
-   100, 500, '2024-10-01');
+   100, 500, '2024-10-01', 6.99);
    
 insert cash
    values('7', 'Cash1', '2023-10-01', 10.99,
-	10.99, 10);
+	1, 10, 10.99);
 insert cash
    values('8', 'Cash2', '2023-8-11', 3.99,
-   5.99, 100);
+   1.2, 100, 4.79);
 insert cash
    values('9', 'Cash3', '2023-8-05', 7.99,
-   6.99, 100);
+   1.4, 100, 11.19);
    
    
    
 insert holdings
-   values('9', 'Cash3', 'cash');
+   values('9', 'Cash3', 'cash', 'EUR');
 insert holdings
-   values('1', 'Stock1', 'stock');
-   insert holdings
-   values('4', 'Bond1', 'bond');
+   values('1', 'Stock1', 'stock', 'AMAZ');
+insert holdings
+   values('4', 'Bond1', 'bond', 'GOOG');
    
 
 
