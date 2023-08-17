1e0d836f3aa7b418edc8b14c1350f70462973b74
Helena <helena@nonsense.com>
2023-08-17 13:51:53 +0000

    create functions

--- app.py
+++ app.py
@@ -8,3 +8,69 @@ db = mysql.connector.connect(
     password="c0nygre",
     database="portfolio"
 )
+
+# Endpoint to create a new stock
+@app.route('/stocks', methods=['POST'])
+def create_stock():
+    id = request.json['id']
+    holdingName = request.json['holdingName']
+    dateOfPurchase = request.json['dateOfPurchase']
+    priceAtPurchase = request.json['priceAtPurchase']
+    qty = request.json['qty']
+    currentPrice = request.json['currentPrice']
+    parValue = request.json['parValue']     
+    maturityDate = request.json['maturityDate']
+
+    cursor = db.cursor()
+   
+    cursor.execute("INSERT INTO stocks (id, holdingName, dateOfPurchase, priceAtPurchase, qty, CurrentPrice, parValue, maturityDate) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)",
+                   (id, holdingName, dateOfPurchase, priceAtPurchase, qty, currentPrice, parValue, maturityDate))
+    db.commit()
+    cursor.close()
+    return jsonify({'message': 'Bond added successfully'})
+
+
+# Endpoint to create a new bond
+@app.route('/bonds', methods=['POST'])
+def create_bond():
+    id = request.json['id']
+    holdingName = request.json['holdingName']
+    dateOfPurchase = request.json['dateOfPurchase']
+    priceAtPurchase = request.json['priceAtPurchase']
+    qty = request.json['qty']
+    currentPrice = request.json['currentPrice']
+
+    cursor = db.cursor()
+   
+    cursor.execute("INSERT INTO bonds (id, holdingName, dateOfPurchase, priceAtPurchase, qty, CurrentPrice) VALUES (%s, %s, %s, %s, %s, %s)",
+                   (id, holdingName, dateOfPurchase, priceAtPurchase, qty, currentPrice))
+    db.commit()
+    cursor.close()
+    return jsonify({'message': 'Bond added successfully'})
+
+
+# Endpoint to create a new cash
+@app.route('/cash', methods=['POST'])
+def create_cash():
+    id = request.json['id']
+    holdingName = request.json['holdingName']
+    dateOfPurchase = request.json['dateOfPurchase']
+    exchAtPurchase = request.json['exchAtPurchase']
+    qty = request.json['qty']
+    exchCurrent = request.json['exchCurrent']
+    currentValue = request.json['currentValue']
+    
+
+    cursor = db.cursor()
+   
+    cursor.execute("INSERT INTO cash (id, holdingName, dateOfPurchase, exchAtPurchase, exchCurrent, qty, currentValue) VALUES (%s, %s, %s, %s, %s, %s, %s)",
+                   (id, holdingName, dateOfPurchase, exchAtPurchase, exchCurrent, qty, currentValue))
+    db.commit()
+    cursor.close()
+    return jsonify({'message': 'Cash added successfully'})
+
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
