ef99f157b6694f0a200276cf93dd1f578754ba15
Izzy <izzy@email.com>
2023-08-24 14:17:35 +0000

    troubleshooting functions

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, jsonify, request
 import mysql.connector
-import random
 import yfinance as yf
 from datetime import date
 from dateutil import relativedelta
@@ -22,6 +21,7 @@ db = mysql.connector.connect(
 # Endpoint to create a new stock
 @app.route('/stocks', methods=['POST'])
 def create_stock():
+    db.reconnect(attempts=1, delay=0)
     id = request.json['id']
     holdingName = request.json['holdingName']
     dateOfPurchase = request.json['dateOfPurchase']
@@ -41,6 +41,7 @@ def create_stock():
 # Endpoint to create a new bond
 @app.route('/bonds', methods=['POST'])
 def create_bond():
+    db.reconnect(attempts=1, delay=0)
     id = request.json['id']
     holdingName = request.json['holdingName']
     dateOfPurchase = request.json['dateOfPurchase']
@@ -65,6 +66,7 @@ def create_bond():
 # Endpoint to create a new cash
 @app.route('/cash', methods=['POST'])
 def create_cash():
+    db.reconnect(attempts=1, delay=0)
     id = request.json['id']
     holdingName = request.json['holdingName']
     dateOfPurchase = request.json['dateOfPurchase']
@@ -86,6 +88,7 @@ def create_cash():
 @app.route('/products/totalvalue', methods=['GET'])
 def gettotalvalue():
     # Calculates the current total value of the portfolio
+    db.reconnect(attempts=1, delay=0)
     cursor = db.cursor()
     cursor.execute('''SELECT SUM(qty*price) as totalValue FROM
                         (SELECT cash.id, cash.qty, cash.exchCurrent AS price FROM cash
@@ -101,6 +104,7 @@ def gettotalvalue():
 @app.route('/products/totalstocks', methods=['GET'])
 def gettotalstocks():
     # Calculates the current total value of all stocks
+    db.reconnect(attempts=1, delay=0)
     cursor = db.cursor()
     cursor.execute('''SELECT SUM(qty*currentPrice) as totalStocks FROM stocks''')
     products = cursor.fetchall()
@@ -111,6 +115,7 @@ def gettotalstocks():
 @app.route('/products/totalbonds', methods=['GET'])
 def gettotalbonds():
     # Calculates the current total value of all bonds
+    db.reconnect(attempts=1, delay=0)
     cursor = db.cursor()
     cursor.execute('''SELECT SUM(qty*currentPrice) as totalBonds FROM bonds''')
     products = cursor.fetchall()
@@ -121,6 +126,7 @@ def gettotalbonds():
 @app.route('/products/totalcash', methods=['GET'])
 def gettotalcash():
     # Calculates the current total value of all cash
+    db.reconnect(attempts=1, delay=0)
     cursor = db.cursor()
     cursor.execute('''SELECT SUM(qty*exchCurrent) as totalCash FROM cash''')
     products = cursor.fetchall()
@@ -131,6 +137,7 @@ def gettotalcash():
 @app.route('/products/initialvalue', methods=['GET'])
 def getinitialvalue():
     # Calculates the initial total value of the portfolio
+    db.reconnect(attempts=1, delay=0)
     cursor = db.cursor()
     cursor.execute('''SELECT SUM(qty*price) as initialValue FROM
                         (SELECT cash.id, cash.qty, cash.exchAtPurchase AS price FROM cash
@@ -144,6 +151,7 @@ def getinitialvalue():
 
 @app.route('/products', methods=['GET'])
 def getAllProducts():
+    db.reconnect(attempts=1, delay=0)
     cursor = db.cursor()
     query = """SELECT id, holdingName, dateOfPurchase, priceAtPurchase, currentPrice, qty FROM stocks UNION 
     SELECT id, holdingName, dateOfPurchase, priceAtPurchase, currentPrice, qty FROM bonds UNION 
@@ -169,7 +177,7 @@ def get_historical_prices(ticker):
 
 @app.route('/products/stocks/<int:item_id>', methods=['DELETE'])
 def delete_stocks(item_id):
-    
+    db.reconnect(attempts=1, delay=0)
     cursor = db.cursor(buffered=True)
     cursor.execute("DELETE FROM stocks WHERE id=%s",
                    (item_id,))
@@ -179,7 +187,7 @@ def delete_stocks(item_id):
 
 @app.route('/products/bonds/<int:item_id>', methods=['DELETE'])
 def delete_bonds(item_id):
-    
+    db.reconnect(attempts=1, delay=0)
     cursor = db.cursor(buffered=True)
     cursor.execute("DELETE FROM bonds WHERE id=%s",
                    (item_id,))
@@ -189,7 +197,7 @@ def delete_bonds(item_id):
 
 @app.route('/products/cash/<int:item_id>', methods=['DELETE'])
 def delete_cash(item_id):
-    
+    db.reconnect(attempts=1, delay=0)
     cursor = db.cursor(buffered=True)
     cursor.execute("DELETE FROM cash WHERE id=%s",
                    (item_id,))
@@ -201,7 +209,7 @@ def delete_cash(item_id):
 # Endpoint to update an existing bond
 @app.route('/bonds/<id>', methods=['PUT'])
 def update_bond(id):
-    
+    db.reconnect(attempts=1, delay=0)
     id = request.json['id']
     holdingName = request.json['holdingName']
     dateOfPurchase = request.json['dateOfPurchase']
@@ -226,7 +234,7 @@ def update_bond(id):
 # Endpoint to update an existing stock
 @app.route('/stocks/<id>', methods=['PUT'])
 def update_stocks(id):
-    
+    db.reconnect(attempts=1, delay=0)
     id = request.json['id']
     holdingName = request.json['holdingName']
     dateOfPurchase = request.json['dateOfPurchase']
@@ -251,7 +259,7 @@ def update_stocks(id):
 # Endpoint to update an existing cash
 @app.route('/cash/<id>', methods=['PUT'])
 def update_cash(id):
-    
+    db.reconnect(attempts=1, delay=0)
     id = request.json['id']
     holdingName = request.json['holdingName']
     dateOfPurchase = request.json['dateOfPurchase']
@@ -276,11 +284,12 @@ def update_cash(id):
 @app.route('/refresh/stocks', methods=['GET'])
 def refreshStocks():
     # get tickers from the stocks table 
+    db.reconnect(attempts=1, delay=0)
     cursor = db.cursor()
     query = "SELECT ticker FROM stocks"
     cursor.execute(query)
     tickers = cursor.fetchall()
-    cursor.close()
+    # cursor.close()
     for i in tickers:
         # get information for this product from yahoo
         ticker = i[0]
@@ -288,25 +297,29 @@ def refreshStocks():
         info = tick.info
         price = info['currentPrice']
         # get the product information from the database using the ticker
-        cursor = db.cursor()
+        # cursor = db.cursor()
         query = "SELECT * FROM stocks WHERE ticker = %s"
         cursor.execute(query, (ticker,))
         product = cursor.fetchall()[0]
+        # cursor.close()
         # # updating the price in the specific product table 
+        # cursor = db.cursor()
         stockQuery = "UPDATE stocks SET currentPrice = %s WHERE id = %s"
         cursor.execute(stockQuery, (price, product[0])) 
         db.commit()
-        cursor.close()
+        # cursor.close()
+    cursor.close()
     return jsonify("Page refreshed successfully")
 
 @app.route('/refresh/cash', methods=['GET'])
 def refreshCash():
     # get tickers from the cash table 
+    db.reconnect(attempts=1, delay=0)
     cursor = db.cursor()
     query = "SELECT ticker FROM cash"
     cursor.execute(query)
     tickers = cursor.fetchall()
-    cursor.close()
+    # cursor.close()
     for i in tickers:
         # get information for this product from yahoo
         ticker = i[0]
@@ -314,38 +327,40 @@ def refreshCash():
         info = tick.info
         rate = info['ask']
         # get the product information from the database using the ticker
-        cursor = db.cursor()
+        # cursor = db.cursor()
         query = "SELECT * FROM cash WHERE ticker = %s";
         cursor.execute(query, (ticker,))
         product = cursor.fetchall()[0]
+        # cursor.close()
         # # updating the price in the specific product table 
+        # cursor = db.cursor()
         cashQuery = "UPDATE cash SET exchCurrent = %s, currentValue=qty*%s WHERE id = %s"
         cursor.execute(cashQuery, (rate, rate, product[0])) 
         db.commit()
-        cursor.close()
+        # cursor.close()
+    cursor.close()
     return jsonify("Page refreshed successfully")
 
 @app.route('/refresh/bonds', methods=['GET'])
 def refreshBonds():
     # Get bonds from table 
+    db.reconnect(attempts=1, delay=0)
     cursor = db.cursor()
     query = "SELECT * FROM bonds"
     cursor.execute(query)
     bonds = cursor.fetchall()
-    cursor.close()
+    # cursor.close()
     for i in bonds:
         # store all necessary values for the calculation
-        purchasePrice = float(i[3])
-        print(type(purchasePrice))
-        randomChange = random.randrange(90,100)/100
-        print(type(randomChange))
-        bondPrice = purchasePrice*randomChange
-        cursor = db.cursor()
+        purchasePrice = i[3]
+        bondPrice = purchasePrice
+        # cursor = db.cursor()
         # update database with the information
         bondQuery = "UPDATE bonds SET currentPrice = %s WHERE id = %s"
         cursor.execute(bondQuery, (bondPrice, i[0])) 
         db.commit()
-        cursor.close()
+        # cursor.close()
+    cursor.close()
     return jsonify("Page refreshed successfully")
             
 if __name__ == '__main__':
